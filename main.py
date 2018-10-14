05909cc0b71017515c07259057ac730f343690dd
Maximiliano Friedl <mnfriedl@uc.cl>
2018-10-14 15:29:26 -0300

    constraint 6

--- main.py
+++ main.py
@@ -10,24 +10,35 @@ X = model.addVars(parameters.volunteers, parameters.locations, vtype=GRB.BINARY,
 Y = model.addVars(parameters.volunteers, parameters.tasks, vtype=GRB.BINARY, name="assigned_to_task")
 W = model.addVars(parameters.volunteers, parameters.tasks, parameters.locations, vtype=GRB.BINARY, name="assigned_to_community_on_task")
 O = model.addVars(parameters.locations, vtype=GRB.BINARY, name="assign_community")
-# a
-# f
-# h
+#? a
+#? f
+#? h
 J = model.addVars(parameters.locations, vtype=GRB.BINARY, name="assigned_to_group")
 
 # Actualización del modelo
 model.update()
 
 # Restricciones
-# Cumplir con el presupuesto
+#! Cumplir con el presupuesto
 model.addConstrs((quicksum(parameters.locations_dict[location]["Costo traslado"] * O[location] for location in parameters.locations) - 
     (1/3)*quicksum(parameters.locations_dict[location]["Costo traslado"] * J[location] for location in parameters.locations if location not in parameters.locations_plane) <= parameters.budget),
     name="r2")
 
 # Tamaño de los grupos
+model.addConstrs((quicksum(X[volunteer][location] for volunteer in parameters.volunteers) <= parameters.locations_dict[location]["Capacidad máxima"] * O[location] for location in parameters.locations), 
+    name="r3")
+model.addConstrs((quicksum(X[volunteer][location] for volunteer in parameters.volunteers) >= parameters.locations_dict[location]["Voluntarios mínimos"] * O[location] for location in parameters.locations), 
+    name="r4")
 
+# Voluntarios pueden pertenecer a una sola misión
+model.addConstrs((quicksum(X[volunteer][location] for volunteer in parameters.volunteers) == 1 for location in parameters.locations),
+    name="r5")
 
-# función objetivo
+# Voluntarios son asignados a una tarea
+model.addConstrs((quicksum(Y[volunteer][task] for task in range(1, 6)) == 1 for volunteer in parameters.volunteers),
+    name="r6")
+
+#! función objetivo
 obj = quicksum(
     parameters.volunteers_info.iloc[volunteer]["hability_{}".format(task)] * Y[volunteer][task]
     for volunteer in parameters.volunteers
